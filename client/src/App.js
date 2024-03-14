a662dfa32e1f3eacb7f6f0e9c1be49b23b089aa8
Jason <murangira99@gmail.com>
2024-03-14 15:43:15 +0200

    added imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Books from "./pages/Books";
-
+import Add from "./pages/Add";
+import Update from "./pages/Update";
 function App() {
   return (
     <div className="App">
