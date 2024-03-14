a79170b5d094fb6d4d91477a8d32cf89117dfebb
Jason <murangira99@gmail.com>
2024-03-14 15:42:17 +0200

    added browser routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Books from "./pages/Books";
 
 function App() {
   return (
     <div className="App">
-      hellow 
+       <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/update" element={<Update />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
