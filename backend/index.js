3550aa6bbe0f0fbf61f0985cb895068afb6a5123
Jason <murangira99@gmail.com>
2024-03-14 12:43:56 +0200

    added app.listen

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,20 @@
 import express from "express";
-//import mysql from "mysql";
+import mysql from "mysql2";
 //import cors from "cors";
 
 const app = express();
+
+// Create a MySQL connection pool
+const pool = mysql.createPool({
+    connectionLimit: 10, // Maximum number of connections in the pool
+    host: "localhost",
+    user: "root",
+    password: "Mur@ngi02",
+    database: "test",
+    debug: false, // Set to true for debugging
+  });
+  
+  app.get("/",(req,res)=>{
+    res.json("hello this is the backend")})
 
 app.listen (8800, ()=>{console.log ( "connected to backend")})
