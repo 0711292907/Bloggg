531357fb8f45227df14cec058c90650c07d7a98e
Jason <murangira99@gmail.com>
2024-03-14 17:42:39 +0200

    added a few staff

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,9 +1,69 @@
-import React from  'react'
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+//import { Link } from "react-router-dom";
 
-const Books = () =>{
-    return(
-        <div>Books</div>
-    )
-}
+const Books = () => {
+  const [books, setBooks] = useState([]);
 
-export default Books
+  useEffect(() => {
+    const fetchAllBooks = async () => {
+      try {
+        const res = await axios.get("http://localhost:8800/books");
+        setBooks(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchAllBooks();
+  }, [])
+
+  return (
+    <div>Books</div>
+  )
+  }    
+  //console.log(books);
+
+  //const handleDelete = async (id) => {
+    //try {
+      //await axios.delete(`http://localhost:8800/books/${id}`);
+      //window.location.reload()
+    //} catch (err) {
+     // console.log(err);
+    //}
+  //};
+
+  //return (
+    //<div>
+      //<h1>Lama Book Shop</h1>
+      //<div className="books">
+       // {books.map((book) => (
+         // <div key={book.id} className="book">
+           // <img src={book.title} alt="" />
+            //<h2>{book.desc}</h2>
+           // <p>{book.cover}</p>
+            //<span>${book.user_name}</span>
+            //<button className="delete" onClick={() => handleDelete(book.id)}>Delete</button>
+            //<button className="update">
+              //<Link
+                //to={`/update/${book.id}`}
+                //style={{ color: "inherit", textDecoration: "none" }}
+              //>
+                //Update
+              //</Link>
+            //</button>
+          //</div>
+        //))}
+      //</div>
+
+      //<button className="addHome">
+       // <Link to="/add" style={{ color: "inherit", textDecoration: "none" }}>
+         // Add new book
+        //</Link>
+      //</button>
+    //</div>
+  //);
+//};
+
+export default Books;
