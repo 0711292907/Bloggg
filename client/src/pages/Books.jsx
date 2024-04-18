3d4afa69a0192a9e7a012f6f1c4ff88713ad0a99
Jason <murangira99@gmail.com>
2024-04-18 11:34:11 +0200

    rempved span

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -36,10 +36,9 @@ const Books = () => {
       <div className="books">
         {books.map((book) => (
           <div key={book.id} className="book">
-            <img src={book.cover} alt="" />
+            
             <h2>{book.title}</h2>
             <p>{book.desc}</p>
-            <span>${book.price}</span>
             <button className="delete" onClick={() => handleDelete(book.id)}>Delete</button>
             <button className="update">
               <Link
